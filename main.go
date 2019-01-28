10ed5b55fefd73119c4b8e0e714d1f2d5efd5340
MGulpen <m.gulpen@live.nl>
2019-01-28 12:02:37 +0100

    added carsysCurrentVersionHandler

--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/home", homeHandler)
+	http.HandleFunc("/carsys/current-version", carsysCurrentVersionHandler)
 	http.ListenAndServe(":12345", nil)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World\n")
 }
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home Page\n")
+func carsysCurrentVersionHandler(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("http://nl.carsys.online/version.json")
+	if err != nil {
+		// handle error
+	}
+	defer resp.Body.Close()
+	body, bodyErr := ioutil.ReadAll(resp.Body)
+	if bodyErr != nil {
+		// handle error
+	}
+	fmt.Fprintf(w, string(body))
 }
