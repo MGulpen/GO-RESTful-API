6ca144c4f78a2af46f2f2a7efaf278f6f6d3439e
MGulpen <m.gulpen@live.nl>
2019-01-28 14:31:04 +0100

    updated comments and struct name

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 /*
 *The main.go file is used to start the RESTful API.
  */
-
 package main
 
 import (
@@ -11,8 +10,8 @@ import (
 	"net/http"
 )
 
-//Message is a template for the json obj from http://nl.carsys.online/version.json
-type Message struct {
+//CarsysResponse is a struct for the json obj from http://nl.carsys.online/version.json
+type CarsysResponse struct {
 	BuiltFromBranch string `json:"Built from branch"`
 	CommitID        string `json:"Commit id"`
 	BuildDate       string `json:"Build date"`
@@ -28,14 +27,17 @@ func main() {
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World\n")
 }
+
+//carsysCurrentVersionHandler presents the date/time stamp from http://nl.carsys.online/version.json
 func carsysCurrentVersionHandler(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get("http://nl.carsys.online/version.json")
 	if err != nil {
 		// handle error
 		panic(err)
 	}
+
+	//the defer tag forces the function to be executed at the end of the function.
 	defer resp.Body.Close()
-	var m Message
 
 	//using io utils to read everything in the body.
 	body, bodyErr := ioutil.ReadAll(resp.Body)
@@ -43,13 +45,14 @@ func carsysCurrentVersionHandler(w http.ResponseWriter, r *http.Request) {
 		// handle error
 		panic(bodyErr)
 	}
-	//using json unmarshal to convert the incoming json object into an object
-	jsonErr := json.Unmarshal(body, &m)
+	//using json unmarshal to convert the incoming json object.
+	var carsysResponse CarsysResponse
+	jsonErr := json.Unmarshal(body, &carsysResponse)
 	if jsonErr != nil {
 		// handle error
 		panic(jsonErr)
 	}
 
 	//print the date/time stamp in the browser.
-	fmt.Fprintf(w, m.BuildDate)
+	fmt.Fprintf(w, carsysResponse.BuildDate)
 }
